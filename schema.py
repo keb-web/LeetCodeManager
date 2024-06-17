20de17ac19bf9686757b7646062cd0df86efbf71
keb-web <kebinhleh@gmail.com>
2024-06-17 05:51:28 -0500

    table restructuring

--- schema.py
+++ schema.py
@@ -1,6 +1,8 @@
 #FIX: adjust program to make QuestionID AutoIncrement ?
 # OR Connect to LC Question DB (JS??)
 
+from datetime import timedelta
+
 QUESTION_BANK = """
 CREATE TABLE IF NOT EXISTS QuestionBank (
     Entry_ID INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -13,29 +15,34 @@ CREATE TABLE IF NOT EXISTS QuestionBank (
 )
 """
 
+#DATETIME instead of DATE
 QUESTION_HISTORY = """
 CREATE TABLE IF NOT EXISTS QuestionHistory (
     History_ID INTEGER PRIMARY KEY AUTOINCREMENT,
     Question_ID INTEGER,
     Completed BOOLEAN NOT NULL,
     Code TEXT,
-    date_attempted DATE,
+    date_attempted DATETIME,
+    retry_date DATETIME,
     Notes TEXT,
     User_ID INTEGER,
     FOREIGN KEY (Question_ID) REFERENCES QuestionBank(Question_ID),
     FOREIGN KEY (User_ID) REFERENCES Users(User_ID)
 )
 """
-# "FOREIGN KEY (User_ID) REFERENCES users(User_ID)"
 
+#TODO: Future feature: Allow for users to adjust RETRY_TIME_WINDOW with a new row stored in "Users" Table
 USERS = """
 CREATE TABLE IF NOT EXISTS Users (
     User_ID INTEGER PRIMARY KEY,
     Total_Completed INTEGER,
-    Total_Attempted INTEGER
+    Total_Attempted INTEGER,
+    Retry_Window INTEGER
 )
 """
 
 USER_ID =  "800858"
 USER_ID2 = "12312"
 # ctx.author.id
+
+RETRY_TIME_WINDOW = timedelta(days=2)
