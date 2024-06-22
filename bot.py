755c3c7cdd33a12a4558f9a79c6bae3942b9aec3
keb-web <kebinhleh@gmail.com>
2024-06-22 17:54:59 -0500

    Insertion and some getter commands

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 # This example requires the 'message_content' intent.
 import discord
+import os
 from discord.ext import commands
 from discord.utils import get
 
 from config import BOT_TOKEN
-# from aioDB ..
+import aioDB as ADB
+from datetime import timedelta, datetime
+from schema import RETRY_TIME_WINDOW
 
 # TODO: Make more verbose
 description = "Bot for tracking LCM"
@@ -14,8 +17,8 @@ intents.members = True # NOTE: (For personalized DMS)
 intents.message_content = True
 
 # client = discord.Client(intents=intents)
-
 bot = commands.Bot(command_prefix='?', description=description, intents = intents)
+
 
 
 # @bot.command(description='For when you wanna settle the score some other way')
@@ -43,8 +46,8 @@ def checkAddParamFormat():
 async def on_message(message):
     if message.author == bot.user:
         return
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello! Use ?help for assistance!')
+    if message.content.startswith('$hello LCM'):
+        await message.channel.send("Hello! Use '?help' for assistance!")
     await bot.process_commands(message)
 
 
@@ -55,48 +58,248 @@ async def on_message(message):
 # [Commands] 
 # TODO: Link discord commands to 'aioDB'
 
-# ?help
+# TODO: fix formatiing
 @bot.command()
 async def helpLCM(ctx):
+    """LCM Bot-Specific Commands"""
     await ctx.send(
-        '''
- Commands:
- ?addQuestion    -> Add Question
-    > @param (str str str str)
-    > ID (Integer)
-    > Name
+'''
+```
+ ?addQuestion <ID>, <NAME>, <DIFFICULTY>, <TYPE>
+    [ Add Question ]
+    > ID         (INT)
+    > Name       (STR)
     > Difficulty ('E'/'M'/'H')
-    > Question Type (Arr, LL, Stack, Queue, Tree, Hashing, BT, Graphs, Tries, Heaps, ..)
- ?updateQuestion -> Updae Question
- ?deleteQuestion -> Delete Question
- ?All            -> Display Question Bank
- ?History QID    -> Display Question, QID's, Submission History
-        '''
-    )
+    > Type       (use ?listTypes for options)
+ ?addSubmission
+    [ Add Question Submission. ]
+ ?changeRetryWindow <DAY>
+    [ Change duration bot waits until notifying user to retry a question (default = 2 days) ]
+    > DAY        (INT)
+ ?updateQuestion    -> [ Update Question ]
+ ?deleteQuestion    -> [ Delete Question ]
+ ?All               -> [ Display Question Bank ]
+ ?History QID       -> [ Display Question, QID's, Submission History ]
+ ?listTypes         -> [ Display Possible Question Types ]
+```
+'''
+)
 
-# ?add
+#TODO: List Question Types
+QUESTION_TYPES = ["ARR", "LL", "HASHING", "TWOPOINTER", "BINARYSEARCH", "SLIDINGWINDOW", "TREES", "TRIES", "BACKTRACKING", "BFS", "DFS", "RECURSION", "HEAPS", "GRAPHS", "GREEDY", "GRAPHS"]
+
+#TODO: Clean up make nicer
+def checkAddQuestionInputFormat(QID: str, NAME: str, DIFF: str, TYPE: str):
+    if not QID or not NAME or not DIFF or not TYPE:
+        print("Not all parameters exist")
+        return False 
+    elif not QID.isdigit():
+        return False
+    elif DIFF.upper() not in ["E", "M", "H"]:
+        print("NOT E/M/H")
+        return False
+    elif TYPE.upper() not in QUESTION_TYPES:
+        print("NOT A VALID TYPE")
+        return False
+    return True
+
+#TODO: Clean up make nicer
+def checkAddSubmissionInputFormat(QID, COMP, CODE, NOTES):
+    if not QID:
+        print("Not all parameters exist")
+        return False 
+    elif not QID.isdigit():
+        print("QID Not digit")
+        return False
+    return True
+
+#Add Character limitaion for NOTES and CODE
+async def promptSub(ctx, QID):
+    if not (await ADB.checkExisting("QuestionBank", "Question_ID", QID)):
+        await ctx.send("Question ID does not exist. Please add it using ?addQuestion")
+        return
+
+    def check(m):
+        return m.channel == ctx.channel and m.author == ctx.author
+    await ctx.send("```Did you complete the Question? (T/F)```") 
+    COMP = True if (await bot.wait_for('message', check=check)).content == "T" else False
+    await ctx.send("```What code did you use? Enter 'x' to skip```") 
+    CODE = ( await bot.wait_for('message', check=check) ).content
+    await ctx.send("```Any Notes? Enter 'x' to skip```") 
+    NOTES = ( await bot.wait_for('message', check=check) ).content
+
+    if CODE.strip().lower() == "x":
+        CODE = ""
+    if NOTES.strip().lower() == "x":
+        NOTES = ""
+
+    retryWindow = await ADB.getRetryWindow(ctx.author.id)
+
+    # x = 0
+    # if retryWindow != None:
+    #     x = datetime.today() + retryWindow
+    # print(f"retryWindow {retryWindow} new retrydate {x}") 
+
+    if not checkAddSubmissionInputFormat(QID, COMP, CODE, NOTES) or retryWindow == None:
+        await ctx.send("Please try again!")
+        return ()
+
+    return (QID, COMP, CODE, datetime.today(), datetime.today() + retryWindow, NOTES, ctx.author.id)
+
 @bot.command()
-async def cmd_AddQuestion(ctx, *questionData: str):
+async def changeRetryWindow(ctx, day):
+    """Update the duration LCM-bot waits until notifying User to retry failed question attempt. (Default duration = 2 days)"""
+    if day.isdigit():
+    #   convertedVal = int(RETRY_TIME_WINDOW.total_seconds())
+        d = int(day)
+        await ADB.updateRetryDate(ctx.author.id, int(timedelta(days=d).total_seconds()))
+
+    print("--- end of changeRetryWindow --- ")
+    await ADB.printAllTables()
+
+
+@bot.command()
+async def listTypes(ctx):
+    """Shows the valid possible question types"""
+    await ctx.send(f"```{QUESTION_TYPES}```")
     pass
 
+@bot.command()
+async def addSubmission(ctx):
+    """Add another attempt to an exisiting question already being tracked by the LCM"""
+
+    def check(m):
+        return m.channel == ctx.channel and m.author == ctx.author
+
+    await ctx.send("```What is the Question ID Number? Enter '?ShowQuestions' to find ID Number```")
+    QID = ( await bot.wait_for('message', check=check) ).content
+    if not QID.isdigit():
+        await ctx.send("Try again!")
+
+    data = await promptSub(ctx, QID)
+    if data == None:
+        return
+
+    await ADB.insertTable("QuestionHistory", data)
+    await ADB.updateUser(ctx.author.id)
+
+    #NOTE: TO REMOVE
+    print("Tables after adding a submission")
+    await ADB.printAllTables()
+
+#FIX: Don't allow user to run command twice if one is still being executed
+@bot.command()
+async def addQuestion(ctx, QID: str, NAME: str, DIFF: str, TYPE: str):
+    """Add Question and Question Attempt for the LCM-bot to track"""
+    if not checkAddQuestionInputFormat(QID, NAME, DIFF, TYPE):
+        await ctx.send("""
+    ```Please try again!
+    ?addQuestion <ID> <NAME> <DIFFICULTY> <TYPE>
+        > ID  :       INT 
+        > Name:       STR
+        > Difficulty: E/M/H
+        > Type:       ?listTypes```
+    """)
+        return
+    
+    #NOTE: Adhere to..
+    # At first Insertion, create user and first submission aswell
+    # Submissions must always have accompanyuing Question In bank
+    # Must always have existing user
+    # No duplicate users
+
+
+    await ADB.insertTable("Users", (ctx.author.id, -1 , -1 , int(RETRY_TIME_WINDOW.total_seconds()) ) )
+    bankData = [int(QID), NAME, DIFF, TYPE, ctx.author.id]
+    await ADB.insertTable("QuestionBank", tuple(bankData))
+    submitData = await promptSub(ctx, QID)
+    if submitData:
+        await ADB.insertTable("QuestionHistory", tuple(submitData))
+    await ADB.updateUser(ctx.author.id)
+
+    print("Tables at end of 'AddQuestion' Command function")
+    await ADB.printAllTables()
+
+# -- [Modiefer Commands] --
 # ?UpdateQuestion
+@bot.command()
 async def cmd_UpdateQuestion(ctx):
     pass
 
 # ?DeleteQuestion
+@bot.command()
 async def cmd_DeleteQuestion(ctx):
     pass
 
-# ?GetUpcoming
-async def cmd_GetUpcoming(ctx):
-    pass
+# -- [Getter Commands] --
+#TODO: 
+# - ?getupcoming, ?todo, ?all, ?completed, ?show <QID>
+
+#TODO: Move helper functiosn and constants into seperate file
+
+qheader, qfooter = ("""
+╔═════╦════════╦════════╦════════════╗
+║ QID ║  NAME  ║  DIFF  ║    TYPE    ║
+╠═════╬════════╬════════╬════════════╣
+""",
+"""
+╚═════╩════════╩════════╩════════════╝
+""")
+
+def questionTableFormatter(items):
+    row_format ="║ {:^4}║ {:6s} ║  {:^4s}  ║ {:^10s} ║"
+    dataString = ""
+    dataString += qheader
+    for item in items:
+        QID, NAME, DIFF, TYPE = item
+        if len(NAME) > 6:
+            NAME = NAME[0:5]
+            NAME += '-'
+        elif len(TYPE) > 10:
+            TYPE = TYPE[0:9]
+            TYPE += '-'
+        dataString += row_format.format(QID, NAME, DIFF, TYPE) + '\n'
+    dataString = dataString.rstrip() # Remove trailing 
+    dataString += qfooter
+    return dataString
+
+
+@bot.command()
+async def getUpcoming(ctx):
+    """Displays previously failed questions"""
+    items = await ADB.getUpcoming(ctx.author.id)
+    await ctx.send("Questions that should be reattempted:")
+    if items == []:
+        ctx.send("Nothing to reattempt :-)")
+    else:
+        await ctx.send(f"```{questionTableFormatter(items)}```")
 
 # ?Todo
-async def cmd_Todo():
+@bot.command()
+async def todo(ctx):
+    """display questions To be retried today"""
+    items = await ADB.getTodoToday(ctx.author.id)
+    await ctx.send("retry these questions today:")
+    if items == []:
+        ctx.send("No questions to redo today :-)")
+    else:
+        await ctx.send(f"```{questionTableFormatter(items)}```")
+
+
+@bot.command()
+async def completed(ctx):
+    "All completed Questions"
     pass
 
-# ?All
-async def DisplayDB(ctx):
+@bot.command()
+async def submissionHistory(ctx, QID):
+    """Question ID's submission history"""
     pass
 
+# async def on_command_error(self, ctx: commands.Context, error):
+#     #Error Handling
+#     if isinstance(error, commands.MissingRequiredArgument):
+#         await ctx.send(f"'{}' is a required argument")
+
+# bot.on_command_error = on_command_error
 bot.run(BOT_TOKEN)
